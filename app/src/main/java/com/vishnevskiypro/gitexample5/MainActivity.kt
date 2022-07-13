eb11b630d9677f94e846bed3742904323739d35f
Oleg144101957 <vishnevskiyoleg@gmail.com>
2022-07-13 22:22:50 +0300

    my first commiy

---
+++ app/src/main/java/com/vishnevskiypro/gitexample5/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.vishnevskiypro.gitexample5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //my app ver 1
+    }
+}
