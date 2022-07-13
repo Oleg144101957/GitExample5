d16b98f16a592ccc4b19926d26e2e8bde1ca05af
Oleg144101957 <vishnevskiyoleg@gmail.com>
2022-07-13 22:38:52 +0300

    my 5 ver

--- app/src/main/java/com/vishnevskiypro/gitexample5/MainActivity.kt
+++ app/src/main/java/com/vishnevskiypro/gitexample5/MainActivity.kt
@@ -8,6 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //my app ver 3
+        //my app ver 5
     }
 }
